65f6802736637b86d1475a9d9513ee358ddb8c17
JoydeepGharami <joydeepgharami@gmail.com>
2025-06-07 23:59:07 +0530

    message

---
+++ src/BookList.js
@@ -0,0 +1,18 @@
+import Book from './Book';
+import React from 'react';
+import books from'./books.json';
+
+ 
+
+const BookList = () => {
+  return (
+    <div>
+      {books.map((ele) =>{
+
+    return  <Book image={ele.image} title={ele.title} publisher={ele.publisher} price={ele.price}/>
+      })}
+    </div>
+  );
+};
+
+export default BookList;
